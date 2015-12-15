62e3114e7e1df805307f5a48eef15235a151d142
Jeremias Ponciano <jere_2001@ig.com.br>
2015-12-15 10:04:05 -0200

    mostra collectionview

--- LinkHub/LinkHub/ViewController.swift
+++ LinkHub/LinkHub/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var CollectionPastas: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.CollectionPastas.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    let pastas = ["a", "b", "c"]
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pastas.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = CollectionPastas.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+        return cell
+    }
+    
+    
 }
 
